cfc97f0d971f2bebcb144b7f4c1791ffe3a82c88
lucianatux <lucianatux@yahoo.com.ar>
2023-05-17 17:46:00 -0300

    inicio calculator

---
+++ script.js
@@ -0,0 +1,11 @@
+function App(){
+    return (
+    <div className="container">
+        <div className="grid">
+            
+        </div>
+    </div>
+    );
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
