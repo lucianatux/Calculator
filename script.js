d7cb806fe89b0b9be4549ce990c2e06875f6df4b
lucianatux <lucianatux@yahoo.com.ar>
2023-05-17 21:25:48 -0300

    funciones

--- script.js
+++ script.js
@@ -1,28 +1,104 @@
 function App() {
-    return (
-      <div className="container">
-        <div className="grid">
-          <div className="padButton dis" id="display"></div>
-          <div className="padButton ac" id="clear">AC</div>
-          <div className="padButton div" id="divide">/</div>
-          <div className="padButton mul" id="multiply">x</div>
-          <div className="padButton seven" id="seven">7</div>
-          <div className="padButton eight" id="eight">8</div>
-          <div className="padButton nine" id="nine">9</div>
-          <div className="padButton sub" id="subtract">-</div>
-          <div className="padButton four" id="four">4</div>
-          <div className="padButton five" id="five">5</div>
-          <div className="padButton six" id="six">6</div>
-          <div className="padButton add" id="add">+</div>
-          <div className="padButton one" id="one">1</div>
-          <div className="padButton two" id="two">2</div>
-          <div className="padButton three" id="three">3</div>
-          <div className="padButton equal" id="equals">=</div>
-          <div className="padButton zero" id="zero">0</div>
-          <div className="padButton point" id="decimal">.</div>
+  const [calculation, setCalculation] = React.useState("");
+  const [result, setResult] = React.useState(""); // Nueva variable de estado para el resultado
+
+  const clearDisplay = () => {
+    setCalculation("");
+    setResult("");
+  };
+
+  const handleNumberClick = (event) => {
+    const number = event.target.textContent;
+    if (result !== "") {
+      setCalculation(number);
+      setResult("");
+    } else {
+      setCalculation((prevCalculation) => prevCalculation + number);
+    }
+  };
+
+  const handleOperatorClick = (event) => {
+    const operator = event.target.textContent;
+    if (result !== "") {
+      setCalculation(result + operator);
+      setResult("");
+    } else {
+      setCalculation((prevCalculation) => prevCalculation + operator);
+    }
+  };
+
+  const handleEqualsClick = () => {
+    let finalResult;
+    try {
+      finalResult = eval(calculation);
+      if (!Number.isFinite(finalResult)) {
+        finalResult = "Error";
+      }
+    } catch (error) {
+      finalResult = "Error";
+    }
+    setCalculation(finalResult.toString());
+    setResult(finalResult.toString()); // Guardar el resultado en la nueva variable de estado
+  };
+
+  return (
+    <div className="container">
+      <div className="grid">
+        <div className="padButton dis display" id="display"><span className="display-text">{calculation}</span></div>
+        <div className="padButton ac" onClick={clearDisplay}>
+          AC
+        </div>
+        <div className="padButton div op" onClick={handleOperatorClick}>
+          /
+        </div>
+        <div className="padButton mul op" onClick={handleOperatorClick}>
+          x
+        </div>
+        <div className="padButton seven" onClick={handleNumberClick}>
+          7
+        </div>
+        <div className="padButton eight" onClick={handleNumberClick}>
+          8
+        </div>
+        <div className="padButton nine" onClick={handleNumberClick}>
+          9
+        </div>
+        <div className="padButton sub op" onClick={handleOperatorClick}>
+          -
+        </div>
+        <div className="padButton four" onClick={handleNumberClick}>
+          4
+        </div>
+        <div className="padButton five" onClick={handleNumberClick}>
+          5
+        </div>
+        <div className="padButton six" onClick={handleNumberClick}>
+          6
+        </div>
+        <div className="padButton add op" onClick={handleOperatorClick}>
+          +
+        </div>
+        <div className="padButton one" onClick={handleNumberClick}>
+          1
+        </div>
+        <div className="padButton two" onClick={handleNumberClick}>
+          2
+        </div>
+        <div className="padButton three" onClick={handleNumberClick}>
+          3
+        </div>
+        <div className="padButton equal" onClick={handleEqualsClick}>
+          =
+        </div>
+        <div className="padButton zero" onClick={handleNumberClick}>
+          0
+          </div>
+        <div className="padButton point" onClick={handleNumberClick}>
+          .
         </div>
       </div>
-    );
-  }
-  
-  ReactDOM.render(<App />, document.getElementById('root'));
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
